21abcef0dd5d2958082bd843c519f1c9588d4842
sargam2198 <40700738+sargam2198@users.noreply.github.com>
2018-06-30 22:00:25 +0530

    Add files via upload

---
+++ page-two.php
@@ -0,0 +1,19 @@
+<?php
+$NAME=$_REQUEST['Name'];
+$AGE=$_REQUEST['Age'];
+$HOME=$_REQUEST['Town'];
+echo <<<TEXT
+<p>Hello, my name is <b>$NAME</b>, I am <b>$AGE</b> years old and live in <b>$HOME</b> and I am learning how to use PHP to create typing shortcuts by passing information from one place to another place within my web pages.
+</p>
+TEXT;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Get_Maps</title>
+</head>
+    <body>
+        <p>Hello Welcome</p>
+        <img src="5_pic.jpg">
+    </body>
+</html>
